ce037d1b1e8f1ad487e5d78949483aa14ed4b666
1 <i_616@mail.ru>
2017-02-03 22:49:04 +0700

    1st commit

---
+++ Shapes/src/ru/academits/makarenko/shapes/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.academits.makarenko.shapes;
+
+/**
+ * Created by 1 on 03.02.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
